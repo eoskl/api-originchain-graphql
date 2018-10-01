81be8d057bf712b6aea644ca882f7a59ca57a1e9
Ryan Pillsbury <ryan@xyo.network>
2018-10-01 12:42:01 -0700

    Updated exports

--- index.d.ts
+++ index.d.ts
@@ -4,11 +4,17 @@
  * @Email:  developer@xyfindables.com
  * @Filename: index.d.ts
  * @Last modified by: ryanxyo
- * @Last modified time: Monday, 1st October 2018 12:17:24 pm
+ * @Last modified time: Monday, 1st October 2018 12:41:42 pm
  * @License: All Rights Reserved
  * @Copyright: Copyright XY | The Findables Company
  */
 
 declare module 'xyo-api-originchain-graphql' {
-  export function getSchema(): Promise<string>
+  export function getSchema(): Promise<XyoOriginChainGraphqlSchema>
+}
+
+interface XyoOriginChainGraphqlSchema {
+  schema: string;
+  queries: {[s: string]: string};
+  types: {[s: string]: string};
 }
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @Email:  developer@xyfindables.com
  * @Filename: index.js
  * @Last modified by: ryanxyo
- * @Last modified time: Monday, 1st October 2018 12:31:37 pm
+ * @Last modified time: Monday, 1st October 2018 12:40:41 pm
  * @License: All Rights Reserved
  * @Copyright: Copyright XY | The Findables Company
  */
@@ -28,19 +28,28 @@ module.exports = (() => {
   return {
     getSchema: () => {
       return files.reduce((promiseChain, fileName) => {
-        return promiseChain.then(schema => {
+        return promiseChain.then(memo => {
             var fileLocation = path.resolve(__dirname, `./graphql/${fileName}`)
             return readFile(fileLocation, 'utf8')
               .then(file => {
-                schema += `\n# ${fileName}\n${file}\n`;
-                return schema;
+                memo.schema += `\n# ${fileName}\n${file}\n`;
+                var isQuery = /query/.test(fileName);
+                if (isQuery) {
+                  memo.queries = memo.queries || {};
+                  memo.queries[fileName] = file;
+                } else {
+                  memo.types = memo.types || {};
+                  memo.types[fileName] = file;
+                }
+
+                return memo;
               })
               .catch(err => {
                 console.error(`There was an error getting file ${fileName}`);
                 throw err;
               })
           })
-      }, Promise.resolve(''));
+      }, Promise.resolve({schema: ''}));
     }
   }
 })();
