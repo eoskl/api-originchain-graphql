61a88581ee1b2286ff92b89fe48f5f2f0e49d477
Ryan Pillsbury <ryan@xyo.network>
2018-10-02 11:04:19 -0700

    Updated package.json and typings

--- index.d.ts
+++ index.d.ts
@@ -4,31 +4,31 @@
  * @Email:  developer@xyfindables.com
  * @Filename: index.d.ts
  * @Last modified by: ryanxyo
- * @Last modified time: Monday, 1st October 2018 3:29:10 pm
+ * @Last modified time: Tuesday, 2nd October 2018 11:03:39 am
  * @License: All Rights Reserved
  * @Copyright: Copyright XY | The Findables Company
  */
 
-declare module 'xyo-api-originchain-graphql' {
-  export function getSchema(): Promise<XyoOriginChainGraphqlSchema | XyoOriginGraphqlNodeV1>
-}
-
-export interface XyoOriginChainGraphqlSchema {
-  schema: string;
-  queries: {[s: string]: string};
-  types: {[s: string]: string};
-}
-
-export interface XyoOriginGraphqlNodeV1 {
-  schema: string;
-  types: {
-    'xyo-blocks-by-public-key.graphql': string,
-    'xyo-block-collection.graphql': string,
-    'xyo-block.graphql': string,
-    'xyo-object-interface.graphql': string,
-    'xyo-payload.graphql': string,
-    'xyo-keyset.graphql': string,
-    'xyo-object-plain.graphql': string,
-    'xyo-signature-set.graphql': string
+declare module '@xyo-network/api-originchain-graphql' {
+  export interface XyoOriginChainGraphqlSchema {
+    schema: string;
+    queries: {[s: string]: string};
+    types: {[s: string]: string};
   }
+  
+  export interface XyoOriginGraphqlNodeV1 {
+    schema: string;
+    types: {
+      'xyo-blocks-by-public-key.graphql': string,
+      'xyo-block-collection.graphql': string,
+      'xyo-block.graphql': string,
+      'xyo-object-interface.graphql': string,
+      'xyo-payload.graphql': string,
+      'xyo-keyset.graphql': string,
+      'xyo-object-plain.graphql': string,
+      'xyo-signature-set.graphql': string
+    }
+  }
+
+  export function getSchema(): Promise<XyoOriginChainGraphqlSchema | XyoOriginGraphqlNodeV1>
 }
